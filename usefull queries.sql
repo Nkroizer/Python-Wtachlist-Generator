32b0ddac9214483ee74e42863e835940b60610e3
Natan <Natan@quickode.com>
2020-07-18 22:47:09 +0300

    weekly dump

--- usefull queries.sql
+++ usefull queries.sql
@@ -1,40 +1,40 @@
-select showName, count(*) as cnt from watchlistdatabase.episodes 
-where verified = 0 and watched = 0
-group by showName order by cnt asc;
+select showName, season, count(*) as cnt from episodes 
+where verified = 0
+group by showName, season order by cnt asc;
 
-select * from episodes where showName = 'The Mandalorian';
+select * from episodes where showName like '%Deep Space Nine%' order by airDate asc;
 
-select * from episodes where showName = 'Saturday Night Live' and season = 15;
+UPDATE episodes SET verified = 1 where showName = 'Saturday Night Live' and season = 45;
+select * from episodes where showName = 'Stargate SG-1' order by airDate asc;
 
-select * from episodes where showName = 'The Simpsons' and season = 4;
+delete from episodes where episodeCode = 'buffythevampireslayerS1E0';
 
-select * from episodes where episodeCode = 'agentsofs.h.i.e.l.d.S7E13';
+select * from episodes where episodeCode = 'yugiohzexalS2E3';
 
-CALL `watchlistdatabase`.`_add_Plus_1_To_Air_Date`('Watchmen');
+CALL `watchlistdatabase`.`_add_Plus_1_To_Air_Date`('Stargate SG-1');
 
-UPDATE episodes SET airDate = '2020-07-15' where episodeCode = 'pokémontwilightwingsS1E6';
+CALL `watchlistdatabase`.`_Remove_Plus_1_To_Air_Date`('Star Trek: Voyager');
 
-UPDATE episodes SET episodeCode = 'saturdaynightliveS10E17' where episodeCode = 'saturdaynightliveS10E18';
+CALL `watchlistdatabase`.`_add_Days_to_To_Episode`('bleachburîchiS1E5', 1);
 
-UPDATE episodes SET airDate = '1984-10-07' where showName = 'Pokémon Origins';
+UPDATE episodes SET airDate = '2001-05-04' where episodeCode = 'survivorS2E15';
 
-UPDATE episodes SET verified = 1 where showName = 'The Mandalorian';
+UPDATE episodes SET season = 2, episode = 3, episodeCode = 'yugiohzexalS2E3' where episodeCode = 'yugiohzexalS-1E-1';
 
-UPDATE episodes SET watched = 1 where showName = 'Inhumans';
+UPDATE episodes SET season = 1, episode = 28, episodeCode = 'yugiohzexalS1E28' where episodeCode = 'yugiohzexalS2E3';
 
-UPDATE episodes SET watched = 1 where showName = 'Pokémon' and season = 10;
+UPDATE episodes SET showName = 'Ripley''s Believe It or Not! (2000)' where showName = 'Ripley''s Believe It or Not!';
 
-UPDATE episodes SET verified = 1 where showName = 'Saturday Night Live' and season = 15;
+UPDATE episodes SET verified = 1 where showName = 'Stargate SG-1';
 
-UPDATE episodes SET verified = 1 where showName = 'The Simpsons' and season = 4;
+UPDATE episodes SET watched = 1 where showName = 'Millennium';
 
 select count(*) as cnt from episodes where verified = 1;
 
-DELETE FROM episodes WHERE episodeCode = 'themandalorianS3E1';
+UPDATE episodes SET title = 'Build the Most Powerful Deck!' where episodeCode = 'thedisastrouslifeofsaikikS3E2';
 
 INSERT INTO episodes (showName, season, episode, title, kind, rating, airDate, year, plot, mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, episodeCode) 
-VALUES ('Pokémon Chronicles', 1, 22, 'Pikachu''s Winter Vacation', 'episode', 0, '2006-11-26', 2006, 'N/A', 1294022, 0, 0, 0, 1, 0, 1, 'pokémonchroniclesS1E22');
-
+VALUES ('The Disastrous Life of Saiki K.', 3, 6, 'Reawakening Saiki Kusuo', 'episode', 0, '2019-12-30', 2019, 'N/A', 6354518, 313435, 0, 0, 0, 0, 1, 'thedisastrouslifeofsaikikS3E6');
 
 select distinct showName, imdbId from episodes where showName not in (select distinct showName from shows);
 select distinct showName from shows where showName not in (select distinct showName from episodes);
@@ -85,7 +85,64 @@ $$
 
 DELIMITER ;
 
-INSERT INTO episodes (showName, season, episode, title, kind, rating, airDate, year, plot, mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, episodeCode) 
-VALUES ('South Park', 24, 1, 'Episode #24.1', 'episode', 0, '1900-01-01', 0, '\n  Know what this is about?\n Be the first one to add a plot.\n    ', 121955, 75897, 0, 0, 0, 0, 0, 'southparkS24E1')
+DROP PROCEDURE IF EXISTS _Remove_Plus_1_To_Air_Date;
+DELIMITER $$
+CREATE PROCEDURE _Remove_Plus_1_To_Air_Date(
+	IN showName VARCHAR(255)
+)
+BEGIN
+   DECLARE cursor_List_isdone BOOLEAN DEFAULT FALSE;
+   DECLARE cur_airDate date;
+   DECLARE cur_code VARCHAR(250) DEFAULT '';
 
+   DECLARE cursor_List CURSOR FOR 
+      select e1.airDate, e1.episodeCode from watchlistdatabase.episodes e1 where e1.showName = showName and e1.verified = 0
+    ;
 
+   DECLARE CONTINUE HANDLER FOR NOT FOUND SET cursor_List_isdone = TRUE;
+
+   OPEN cursor_List;
+
+   loop_List: LOOP
+      FETCH cursor_List INTO cur_airDate, cur_code;
+      IF cursor_List_isdone THEN
+         LEAVE loop_List;
+      END IF;
+
+      UPDATE watchlistdatabase.episodes es SET es.airDate = DATE_ADD(cur_airDate, INTERVAL -1 DAY) WHERE es.episodeCode = cur_code;
+
+   END LOOP loop_List;
+
+   CLOSE cursor_List;
+END
+
+$$
+
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS _add_Days_to_To_Episode;
+DELIMITER $$
+CREATE PROCEDURE _add_Days_to_To_Episode(
+	IN episodeCodeFunc VARCHAR(255),
+    days int
+)
+BEGIN
+	SELECT 
+		airDate
+	INTO
+		@airDate
+	FROM 
+		watchlistdatabase.episodes
+	WHERE 
+		episodeCode = episodeCodeFunc;
+    
+      UPDATE watchlistdatabase.episodes es SET es.airDate = DATE_ADD(@airDate, INTERVAL days DAY) WHERE es.episodeCode = episodeCodeFunc;
+      
+      select * from episodes WHERE episodeCode = episodeCodeFunc;
+END
+
+$$
+
+DELIMITER ;
+
+ALTER TABLE episodes MODIFY episodeCode VARCHAR(255) UNIQUE NOT NULL;
