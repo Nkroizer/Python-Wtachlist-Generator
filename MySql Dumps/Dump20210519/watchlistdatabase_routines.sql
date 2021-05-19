d607201b9b6b5398c4b1f031ecb0d327d8cb7931
Nathan Kroizer <42532605+Nkroizer@users.noreply.github.com>
2021-05-19 14:26:46 +0300

    MySql connection fix + dump

---
+++ MySql Dumps/Dump20210519/watchlistdatabase_routines.sql
@@ -0,0 +1,150 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: watchlistdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping routines for database 'watchlistdatabase'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `_add_Days_to_To_Episode` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `_add_Days_to_To_Episode`(
+	IN episodeCodeFunc VARCHAR(255),
+    days int
+)
+BEGIN
+	SELECT 
+		airDate
+	INTO
+		@airDate
+	FROM 
+		watchlistdatabase.episodes
+	WHERE 
+		episodeCode = episodeCodeFunc;
+    
+      UPDATE watchlistdatabase.episodes es SET es.airDate = DATE_ADD(@airDate, INTERVAL days DAY) WHERE es.episodeCode = episodeCodeFunc;
+      
+      select * from episodes WHERE episodeCode = episodeCodeFunc;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `_add_Plus_1_To_Air_Date` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `_add_Plus_1_To_Air_Date`(
+	IN showName VARCHAR(255)
+)
+BEGIN
+   DECLARE cursor_List_isdone BOOLEAN DEFAULT FALSE;
+   DECLARE cur_airDate date;
+   DECLARE cur_code VARCHAR(250) DEFAULT '';
+
+   DECLARE cursor_List CURSOR FOR 
+      select e1.airDate, e1.episodeCode from watchlistdatabase.episodes e1 where e1.showName = showName and e1.wasIncremented = 0 and e1.verified = 0
+    ;
+
+   DECLARE CONTINUE HANDLER FOR NOT FOUND SET cursor_List_isdone = TRUE;
+
+   OPEN cursor_List;
+
+   loop_List: LOOP
+      FETCH cursor_List INTO cur_airDate, cur_code;
+      IF cursor_List_isdone THEN
+         LEAVE loop_List;
+      END IF;
+
+      UPDATE watchlistdatabase.episodes es SET es.airDate = DATE_ADD(cur_airDate, INTERVAL 1 DAY), es.wasIncremented = 1 WHERE es.episodeCode = cur_code;
+
+   END LOOP loop_List;
+
+   CLOSE cursor_List;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `_Remove_Plus_1_To_Air_Date` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `_Remove_Plus_1_To_Air_Date`(
+	IN showName VARCHAR(255)
+)
+BEGIN
+   DECLARE cursor_List_isdone BOOLEAN DEFAULT FALSE;
+   DECLARE cur_airDate date;
+   DECLARE cur_code VARCHAR(250) DEFAULT '';
+
+   DECLARE cursor_List CURSOR FOR 
+      select e1.airDate, e1.episodeCode from watchlistdatabase.episodes e1 where e1.showName = showName and e1.verified = 0
+    ;
+
+   DECLARE CONTINUE HANDLER FOR NOT FOUND SET cursor_List_isdone = TRUE;
+
+   OPEN cursor_List;
+
+   loop_List: LOOP
+      FETCH cursor_List INTO cur_airDate, cur_code;
+      IF cursor_List_isdone THEN
+         LEAVE loop_List;
+      END IF;
+
+      UPDATE watchlistdatabase.episodes es SET es.airDate = DATE_ADD(cur_airDate, INTERVAL -1 DAY) WHERE es.episodeCode = cur_code;
+
+   END LOOP loop_List;
+
+   CLOSE cursor_List;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-19 14:23:47
--- pythonWtachListGenerator/watchListGenerator/pythonToMySqlConnection.py
+++ pythonWtachListGenerator/watchListGenerator/pythonToMySqlConnection.py
@@ -4,6 +4,7 @@ from imdb import IMDb
 import requests
 import json
 import pythonWtachListGenerator.watchListGenerator.conversionFunctions as conversionFunctions
+#import conversionFunctions as conversionFunctions
 
 
 def responseToData(response):
@@ -92,6 +93,7 @@ def getCleanShowName(showName):
     cleanName = cleanName.replace("!", "")
     cleanName = cleanName.replace("?", "")
     cleanName = cleanName.replace(".", "")
+    cleanName = cleanName.replace("'", "")
     cleanName = cleanName.lower()
     return cleanName
 
@@ -119,7 +121,10 @@ def insertSingleShowRecord(imdbId):
     plot = plot[0: 1499]
     coverUrl = series["cover url"]
     fullSizeCoverUrl = series["full-size cover url"]
-    ShowTitle = series["original title"]
+    try:
+        ShowTitle = series["original title"]
+    except:
+        ShowTitle = series["title"]
     lastDig = ShowTitle[len(ShowTitle) - 3: len(ShowTitle) - 2]
     active = 0
     if lastDig == '-':
@@ -160,35 +165,61 @@ def insertSingleEpisodeRecord(imdbIdIns):
     showName = series["title"]
     ia.update(series, "episodes")
     SeasonsArr = sorted(series["episodes"].keys())
+    firstYear = 0
+    latestSeason = 0
+    mycursor.execute("select season from episodes where mainImdbId = '" + str(imdbIdIns) + "' and verified = 1 order by season, episode desc LIMIT 1")
+    myresult = mycursor.fetchall()
+    if len(myresult) > 0:
+        latestSeason = myresult[0][0]
     for SeasonNum in SeasonsArr:
-        seasonx = series["episodes"][SeasonNum]
-        EpisodeArr = sorted(seasonx)
-        for episodz in EpisodeArr:
-            episode_obj = series["episodes"][SeasonNum][episodz]
-            season = episode_obj["season"]
-            episode = episode_obj["episode"]
-            title = episode_obj["title"]
-            kind = episode_obj["kind"]
-            rating = getRating(episode_obj)
-            airDate = getAirDate(episode_obj)
-            year = getYear(episode_obj)
-            plot = episode_obj["plot"]
-            imdbId = 0
-            tvdbId = 0
-            watched = 0
-            wasIncremented = 0
-            verified = 0
-            episodeCode = getCleanShowName(
-                showName) + "S" + str(season) + "E" + str(episode)
-            sql = "INSERT INTO episodes (showName, season, episode, title, kind, rating, airDate, year, plot, mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, episodeCode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            val = (str(showName), season, episode, str(title), str(kind),
-                   rating, airDate, year, str(plot), mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, str(episodeCode))
-            try:
-                mycursor.execute(sql, val)
-                mydb.commit()
-            except:
-                print("E?")
-                print(val)
+        if SeasonNum >= latestSeason: 
+            seasonx = series["episodes"][SeasonNum]
+            EpisodeArr = sorted(seasonx)
+            for episodz in EpisodeArr:
+                episode_obj = series["episodes"][SeasonNum][episodz]
+                season = episode_obj["season"]
+                if not season == -1:
+                    if season < 10:
+                        fixedSeason = "0" + str(season)
+                    else:
+                        fixedSeason = str(season)
+                    episode = episode_obj["episode"]
+                    if not episode == 0:
+                        if episode < 10:
+                            fixedEpisode = "0" + str(episode)
+                        else:
+                            fixedEpisode = str(episode)
+                        title = episode_obj["title"]
+                        kind = episode_obj["kind"]
+                        rating = getRating(episode_obj)
+                        airDate = getAirDate(episode_obj)
+                        year = getYear(episode_obj)
+                        if firstYear == 0:
+                            firstYear = year
+                        plot = episode_obj["plot"]
+                        imdbId = 0
+                        tvdbId = 0
+                        watched = 0
+                        wasIncremented = 0
+                        verified = 0
+                        episodeCode = getCleanShowName(showName) + "(" + str(firstYear) + ")" + "S" + fixedSeason + "E" + fixedEpisode
+                        fullShowName =  str(showName) + " (" + str(firstYear) + ")"
+                        print(episodeCode)
+                        mycursor.execute("SELECT * FROM episodes WHERE episodeCode = '" + str(episodeCode) + "'")
+                        myresult = mycursor.fetchall()
+                        if((len(myresult) > 0) and (myresult[0][17] == 1)):
+                            print("entry allready exists and verified")
+                        else:
+                            sql = "INSERT INTO episodes (showName, season, episode, title, kind, rating, airDate, year, plot, canonUniverse, nonCanonUniverse, mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, episodeCode) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                            val = ( str(fullShowName), season, episode, str(title), str(kind),
+                                rating, airDate, year, str(plot), "", "", mainImdbId, mainTvdbId, imdbId, tvdbId, watched, wasIncremented, verified, str(episodeCode))
+                            try:
+                                mycursor.execute(sql, val)
+                                mydb.commit()
+                                print("Done with: " +str(episodeCode))
+                            except:
+                                print("E?")
+                                print(val)
     print("finished with " + str(showName))
 
 
@@ -245,6 +276,31 @@ def ImdbFileToDb():
                 insertSingleEpisodeRecord(x)
 
 
+def forceUpdateEpisodesYear(imdbIdIns, showName, year):
+    mycursor.execute("SELECT  * FROM episodes WHERE mainImdbId = '" + str(imdbIdIns) +"'")
+    myresult = mycursor.fetchall()
+    for i in range(len(myresult)):
+        row = myresult[i]
+        season = row[1]
+        print("Season: " + str(season))
+        if season < 10:
+            fixedSeason = "0" + str(season)
+        else:
+            fixedSeason = str(season)
+        episode = row[2]
+        print("Episode: " + str(episode))
+        if episode < 10:
+            fixedEpisode = "0" + str(episode)
+        else:
+            fixedEpisode = str(episode)
+        oldEpisodeCode = row[len(row) - 1]
+        episodeCode = getCleanShowName(showName) + "(" + str(year) + ")" + "S" + fixedSeason + "E" + fixedEpisode
+        fullShowName =  str(showName) + " (" + str(year) + ")"
+        mycursor.execute("UPDATE episodes SET showName = '" + str(fullShowName) + "', episodeCode = '" + str(episodeCode) + "' WHERE episodeCode = '" + str(oldEpisodeCode) + "'")
+        print("episode code :" + str(oldEpisodeCode) + " Updated to " + str(episodeCode))
+        mydb.commit()
+    
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -253,6 +309,7 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+# forceUpdateEpisodesYear(168366, "Pokémon" ,1997)
 
 # mycursor.execute("SELECT imdbId FROM shows")
 
@@ -263,4 +320,4 @@ mycursor = mydb.cursor()
 
 # insertAllShowRecords()
 # ImdbFileToDb()
-# insertSingleEpisodeRecord("0318252")
+# insertSingleEpisodeRecord('168366')
